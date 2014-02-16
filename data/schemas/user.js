a26b536164b86e31dedf3fd6417d5c3493a93ae1
bnviki <bn.vikram@gmail.com>
2014-02-16 19:13:16 +0530

    strophe room connection, chat room concept impl

--- data/schemas/user.js
+++ data/schemas/user.js
@@ -1,50 +1,62 @@
 var mongoose = require('mongoose'),
-    findOrCreate = require('mongoose-findorcreate');
+    findOrCreate = require('mongoose-findorcreate') ;
 
 var emailRegexp = /.+\@.+\..+/;
 
 var UserSchema = new mongoose.Schema({
-	email: {
-		type:String, 
-		unique: true, 
-		required: true, 
-		match: emailRegexp, 
-		trim: true},
-	username: {
-		   type:String, 
-		   unique: true,		    
-		   trim: true},
-	displayname: {
-		   type:String, 		   
-		   trim: true},
-	password: {
-		   type:String, 		   
-		   trim: true},
-	extid: String,
-	gender: {
-		type: String,
-		uppercase: true,
-		'enum': ['M', 'F', 'NS']},
-	picture: String,
-    interests: {
-		   	categories: [mongoose.Schema.Types.ObjectId]},
-    status_message: {type: String, default: 'Hi there, lets talk!!'},
-    signup_complete : {type: Boolean, default: false},
-	validated: {
-		     type: Boolean,
-		     default: false},
-	created_at: {
-			type: Date,
-			default: Date.now,
-			set: function(val) {
-				return undefined;
-			}
-		    }	
+    email: {
+        type:String,
+        unique: true,
+        required: true,
+        match: emailRegexp,
+        trim: true},
+    location: {
+        type:String,
+        trim: true},
+    username: {
+        type:String,
+        required: true,
+        unique: true,
+        trim: true},
+    displayname: {
+        type:String,
+        trim: true},
+    password: {
+        type:String,
+        required: true,
+        trim: true},
+    usertype: {
+        type:String,
+        required: true,
+        trim: true},
+
+    picture: {
+        type: String,
+        default: 'http://localhost:3000/profile/pictures/guest.png'
+    },
+    category: {type: mongoose.Schema.Types.ObjectId},
+    description: {type: String, required: true},
+    about: {type: String},
+    phone: {type: String},
+    address: {type: String},
+    chats: [{type: mongoose.Schema.Types.ObjectId, ref: 'Chat'}],
+
+    validated: {
+        type: Boolean,
+        default: false},
+    lastlogin: {
+        type: Date
+    },
+
+    created_at: {
+        type: Date
+    }
+
 
 });
 
 if (!UserSchema.options.toJSON) UserSchema.options.toJSON = {};
-UserSchema.options.toJSON.hide = 'password extid signup_complete validated created_at __v';
+UserSchema.options.toJSON.hide = 'validated created_at __v';
 UserSchema.options.toJSON.transform = function (doc, ret, options) {
     if (options.hide) {
         options.hide.split(' ').forEach(function (prop) {
@@ -54,17 +66,23 @@ UserSchema.options.toJSON.transform = function (doc, ret, options) {
 }
 
 UserSchema.methods.validatePassword = function(password){
-	if(this.password === password)
-		return true;
-	else
-		return false;
+    if(this.password === password)
+        return true;
+    else
+        return false;
 }
 
-UserSchema.pre('save', function(next) {	
-	if (this.isNew) {
-		this.created_at = Date.now;
-	}	
-	next();
+UserSchema.methods.setLastLogin = function(){
+    this.lastlogin = Date.now();
+}
+
+
+UserSchema.pre('save', function(next) {
+    if (this.isNew) {
+        this.created_at = Date.now();
+        this.chats = [];
+    }
+    next();
 });
 
 UserSchema.plugin(findOrCreate);
